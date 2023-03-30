de2c68fe21fae7534104491750e7a980674b84f2
Anastasia89038 <59653663+Anastasia89038@users.noreply.github.com>
2023-03-30 22:08:28 +0400

    класс Dog

--- qaselenium/PythonSite/шаблон_u_tests.py
+++ qaselenium/PythonSite/шаблон_u_tests.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import unittest
 import time
-
+#
 class TestMainPage(unittest.TestCase):
 
     def setUp(self):#перед началом каждого теста

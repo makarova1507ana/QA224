e761c220f678fc39d80b9f14770c033dde6b11f9
Anastasia89038 <59653663+Anastasia89038@users.noreply.github.com>
2023-03-30 21:46:45 +0400

    шаблон unuttest

---
+++ qaselenium/PythonSite/шаблон_u_tests.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import unittest
+import time
+
+class TestMainPage(unittest.TestCase):
+
+    def setUp(self):#перед началом каждого теста
+        pass #заглушка
+
+    def test_case(self):
+        pass
+
+    def tearDown(self):# после каждого теста
+        pass
